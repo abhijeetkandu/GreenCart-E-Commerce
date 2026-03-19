8d38ad5d23d3cbbd51e32536588d908fed7e285a
abhijeetkandu <kanduabhi9408@gmail.com>
2026-03-19 22:22:52 +0530

    Update DbConnection.java

--- src/main/java/com/ecommerce/model/DbConnection.java
+++ src/main/java/com/ecommerce/model/DbConnection.java
@@ -1,18 +1,26 @@
-
 package com.ecommerce.model;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
 public class DbConnection {
-    public static Connection getConnection() throws ClassNotFoundException, SQLException{
-             
+
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce_db","root","root");
-        //System.out.println("Connection Built Successfull");
+
+        String url = "jdbc:mysql://sql12.freesqldatabase.com:3306/sql12820648";
+        String username = "sql12820648";
+        String password = "D95vdyh6lQ";
+
+        Connection conn = DriverManager.getConnection(url, username, password);
+
+        System.out.println("Connected Successfully ✅");
         return conn;
     }
-    public static void main(String[] args) throws ClassNotFoundException, SQLException{
-        //System.out.println(getConnection());
+
+    public static void main(String[] args) throws Exception {
+        getConnection();
     }
-    
 }
