1861be32e362c2cea35d6eb6be09bfaaa7aae680
Abhijeet <kanduabhi9408@gmail.com>
2026-03-29 17:50:36 +0530

    changes have done.

---
+++ src/main/java/com/ecommerce/controllers/LogoutServlet.java
@@ -0,0 +1,17 @@
+package com.ecommerce.controllers;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+        resp.sendRedirect(req.getContextPath()+"/views/admin/adminlogin.jsp");
+    }
+}
